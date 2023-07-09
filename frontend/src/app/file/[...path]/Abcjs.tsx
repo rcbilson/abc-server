3a5392b67ff5011a809aa83299e2f24986de3e7c
Richard Bilson <rcbilson@gmail.com>
2023-07-09 08:56:42 +0000

    factor out synth init; add pause,resume

--- frontend/src/app/file/[...path]/Abcjs.tsx
+++ frontend/src/app/file/[...path]/Abcjs.tsx
@@ -24,76 +24,123 @@ const Abcjs: React.FC<AbcProps> = (props: AbcProps) => {
   const {abcNotation, parserParams, engraverParams, renderParams} = propsWithDefaults;
 
   const uniqueNumber = useRef(Date.now() + Math.random());
-  const abcjsState = useRef();
-  const [playing, setPlaying] = useState(false);
-  const [audioSupported, setAudioSupported] = useState(true);
+  const audioContext = useRef();
   const midiBuffer = useRef();
+  const [playing, setPlaying] = useState(false);
+  const [paused, setPaused] = useState(false);
+  const [audioError, setAudioError] = useState();
+  const [abcjsState, setAbcjsState] = useState();
 
   useEffect(() => {
-    abcjsState.current = ABCJS.renderAbc(
+    setAbcjsState(ABCJS.renderAbc(
       'abcjs-result-' + uniqueNumber.current,
       abcNotation,
       parserParams,
       engraverParams,
       renderParams
-    )[0];
+    )[0]);
+    midiBuffer.current = null;
   }, [abcNotation, parserParams, engraverParams, renderParams]);
 
-  const onEndedCallback = () => {
-    setPlaying(false);
-  }
-
-  const togglePlayback = async () => {
-    if (!abcjsState.current) return;
-
-    // This call involves instantiating an audio context, which you can only really do in a
-    // click handler. So we assume that audio is supported but might discover after a click
-    // that it is not.
+  const midiBufferInit = async () => {
     if (!ABCJS.synth.supportsAudio()) {
-      setAudioSupported(false);
+      setAudioError("Playback is not supported on this browser.");
       return;
     }
 
-    if (!playing) {
-      try {
-        midiBuffer.current = new ABCJS.synth.CreateSynth();
+    try {
+      if (!audioContext.current) {
+        audioContext.current = new AudioContext();
+      }
+      const synth = new ABCJS.synth.CreateSynth();
 
-        // midiBuffer.init preloads and caches all the notes needed. There may be significant network traffic here.
-        await midiBuffer.current.init({
-                visualObj: abcjsState.current,
-                options: {
-                  onEnded: onEndedCallback
-                }
-        });
-        // midiBuffer.prime actually builds the output buffer.
-        await midiBuffer.current.prime();
-        // At this point, everything slow has happened. midiBuffer.start will return very quickly and will start playing very quickly without lag.
-        await midiBuffer.current.start();
-        setPlaying(true);
-      } catch(error) {
-        console.warn("synth error", error);
-      }
-    } else { // !playing
-      if (midiBuffer.current) {
-        midiBuffer.current.stop();
-      }
+      // midiBuffer.init preloads and caches all the notes needed. There may be significant network traffic here.
+      await synth.init({
+        visualObj: abcjsState,
+        options: {
+          onEnded: onEndedCallback
+        }
+      });
+      // midiBuffer.prime actually builds the output buffer.
+      await synth.prime();
+      midiBuffer.current = synth;
+      setAudioError();
+    } catch(error) {
+      midiBuffer.current = undefined;
+      setAudioError(error);
+    }
+  }
+
+  const onEndedCallback = () => {
+    if (!paused) {
       setPlaying(false);
     }
   }
 
-  let playbackButton = <p>Playback is not supported on this browser.</p>;
-  if (audioSupported) {
-    let playbackText = "Start playback";
-    if (playing) {
-      playbackText = "Stop playback";
+  const onPlay = async () => {
+    if (playing) return;
+    if (!midiBuffer.current) {
+      await midiBufferInit();
+      if (!midiBuffer.current) {
+        return;
+      }
     }
-    playbackButton = <button onClick={togglePlayback}>{playbackText}</button>;
+
+    try {
+      // At this point, everything slow has happened. midiBuffer.start will return very quickly and will start playing very quickly without lag.
+      await midiBuffer.current.start();
+      setPlaying(true);
+      setPaused(false);
+    } catch(error) {
+      console.warn("synth error", error);
+    }
+  }
+
+  const onStop = async () => {
+    if (!playing) return;
+    if (midiBuffer.current) {
+      midiBuffer.current.stop();
+    }
+    setPlaying(false);
+    setPaused(false);
+  }
+
+  const onPause = async () => {
+    if (!playing || paused) return;
+    if (midiBuffer.current) {
+      midiBuffer.current.pause();
+    }
+    setPaused(true);
+  }
+
+  const onResume = async () => {
+    if (!playing || !paused) return;
+    if (midiBuffer.current) {
+      midiBuffer.current.resume();
+    }
+    setPaused(false);
+  }
+
+  let playbackElements = [];
+  if (audioError) {
+    playbackElements.push(<p>{audioError}</p>);
+  } else {
+    if (!playing) {
+      playbackElements.push(<button key="play" onClick={onPlay}>Play</button>);
+    } else {
+      playbackElements.push(<button key="stop" onClick={onStop}>Stop</button>);
+      if (paused) {
+        playbackElements.push(<button key="resume" onClick={onResume}>Resume</button>);
+      } else {
+        playbackElements.push(<button key="pause" onClick={onPause}>Pause</button>);
+      }
+    }
   }
 
   return (
     <div style={{ width: '100%' }}>
       <div id={'abcjs-result-' + uniqueNumber.current} style={{ width: '100%' }} />
-      <div>{playbackButton}</div>
+      <div>{playbackElements}</div>
     </div>
   )
 }
