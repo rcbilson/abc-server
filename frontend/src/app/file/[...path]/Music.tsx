15a3fba54626115851f8ce97ec3c2121a2147f6e
Richard Bilson <rcbilson@gmail.com>
2023-07-13 00:57:12 +0000

    use tailscale ip

--- frontend/src/app/file/[...path]/Music.tsx
+++ frontend/src/app/file/[...path]/Music.tsx
@@ -5,7 +5,8 @@ import Abcjs from "./Abcjs";
 
 export default function Music(params: { path: string }) {
   let body = (<p>"nothing yet..."</p>);
-  const data = useServerSentEvents('http://192.168.10.3:9000/' + params.path);
+  //const data = useServerSentEvents('http://192.168.10.3:9000/' + params.path);
+  const data = useServerSentEvents('http://100.89.200.9:9000/' + params.path);
   if (data) {
     body = (
       <Abcjs
